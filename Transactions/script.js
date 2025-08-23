20f2dc1f406228cd8793b65444125b09a6a0c7e5
ElenaGQA <elena.grebeniuk@gmail.com>
2025-08-22 21:24:55 -0400

    update

--- Transactions/script.js
+++ Transactions/script.js
@@ -14,11 +14,31 @@ const transactions = [
     { description: "Coffee", amount: 15 }
 ];
 
+btn.disabled = true;
+
+input.addEventListener('input', ()=>{
+    btn.disabled = true;
+    if(input.value == "" || input.value <1 ){
+        message.textContent = " Please, enter a valid number"
+        message.style.color = "red"  
+    }
+    else if(input.value > transactions.filter((el) =>
+        el.amount > 100
+    ).length){
+        message.textContent = ` Please, enter a valid number. Maximum transaction count is ${transactions.filter((el) =>el.amount > 100).length}`
+        message.style.color = "red"
+    }
+    else{
+        console.log("test")
+        btn.disabled = false;
+    }
+
+})
 
 btn.addEventListener('click', () => {
     let filteredTransactions = transactions.filter((el) =>
         el.amount > 100
-    ).sort((a, b) => b.amount - a.amount).slice(0, 3)
+    ).sort((a, b) => b.amount - a.amount).slice(0, input.value)
 
     res.innerHTML = ""
 
